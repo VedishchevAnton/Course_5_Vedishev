9675e149299de36813df549ffd1663ebed6bccb2
Anton Vedischev <vedishchevanton@gmail.com>
2023-05-08 19:28:00 +0300

    изменение DBManager

--- src/sql/dbmanager.py
+++ src/sql/dbmanager.py
@@ -1,28 +1,19 @@
 import psycopg2
-import os
+from src.sql.config import user, password
 
 
 class DBManager:
-    # Получение данных для подключения к базе данных из переменных окружения
-    database = os.environ.get("DB_NAME")
-    user = os.environ.get("DB_USER")
-    password = os.environ.get("DB_PASSWORD")
-    host = os.environ.get("DB_HOST")
-    port = os.environ.get("DB_PORT")
-
-    def __init__(self):
+    def __init__(self, database_name: str):
         # Установка соединения с базой данных с использованием полученных данных
-        self.conn = psycopg2.connect(database=self.database, user=self.user, password=self.password, host=self.host,
-                                     port=self.port)
-        # Включение автоматической фиксации транзакций
-        # self.conn.autocommit = True
+        self._database = database_name
 
     def get_companies_and_vacancies_count(self):
         """
         Функция получает список всех компаний и количество вакансий у каждой компании
         """
 
-        cur = self.conn.cursor()
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
         cur.execute(
             "SELECT e.name AS company_name, COUNT(v.vacancy_id) AS vacancies_count "
             "FROM employers e LEFT JOIN vacancies v ON e.employer_id = v.employer_id "
@@ -33,54 +24,70 @@ class DBManager:
         for row in rows:
             print(f"{row}: {row} vacancies")
         cur.close()
-        self.conn.close()
+        conn.close()
 
     def get_all_vacancies(self):
         """
         Функция получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на
         вакансию
         """
-        cursor = self.conn.cursor()
-        cursor.execute('''
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
+        cur.execute('''
         SELECT e.name AS company_name, v.name AS vacancy_name, v.salary_from, v.salary_to, v.currency, v.url
         FROM employers e
         JOIN vacancies v ON e.employer_id = v.employer_id;
         ''')
-        all_vacancies = cursor.fetchall()
-        cursor.close()
-        return all_vacancies
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
 
     def get_avg_salary(self):
         """
         Функция получает среднюю зарплату по вакансиям
         """
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT AVG((salary_from + salary_to) / 2) as average_salary "
-                       "FROM vacancies;")
-        result = cursor.fetchone()
-        cursor.close()
-        return result
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
+        cur.execute("SELECT AVG((salary_from + salary_to) / 2) as average_salary FROM vacancies;")
+        rows = cur.fetchall()
+        print(f"Среднее значение 'зарплаты от': {rows}")
+        cur.close()
+        conn.close()
 
     def get_vacancies_with_higher_salary(self):
         """Функция получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
-        cursor = self.conn.cursor()
-        cursor.execute("""
-        SELECT * FROM vacancies
-        WHERE (salary_from + salary_to) / 2 > (
-            SELECT AVG((salary_from + salary_to) / 2) FROM vacancies
-        );
-        """)
-        result = cursor.fetchall()
-        cursor.close()
-        return result
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM vacancies WHERE salary_from > (SELECT AVG(salary_from) FROM vacancies)")
+        rows = cur.fetchall()
+        for row in rows:
+            print(f"id: {row[0]} Вакансия: {row[2], row[3], row[4], row[5], row[6]}")
+        cur.close()
+        conn.close()
 
     def get_vacancies_with_keyword(self, keyword):
         """
         Функция получает список всех вакансий, в названии которых содержатся переданные в метод слова
         """
-        cursor = self.conn.cursor()
-        cursor.execute(f"""SELECT * FROM vacancies 
-        WHERE name LIKE %{keyword}%;""")
-        result = cursor.fetchall()
-        cursor.close()
-        return result
+        conn = psycopg2.connect(host="localhost", database=self._database, user=user, password=password)
+        cur = conn.cursor()
+        sql_query = "SELECT * FROM vacancies WHERE name LIKE %s"  # Формируем SQL запрос
+        search_params = (f'%{keyword}%',)  # Задаем параметры поиска
+        cur.execute(sql_query, search_params)  # Выполняем запрос с параметрам
+        result = cur.fetchall()
+        for row in result:
+            print(f"id: {row[0]} Вакансия: {row[2], row[3], row[4], row[5], row[6]}")
+        cur.close()
+        conn.close()
+
+
+# Пример использования функции
+# test = DBManager('main_db')
+# test.get_companies_and_vacancies_count()
+# test.get_all_vacancies()
+# test.get_avg_salary()
+# test.get_vacancies_with_higher_salary()
+# test.get_vacancies_with_keyword('инженер')
+
